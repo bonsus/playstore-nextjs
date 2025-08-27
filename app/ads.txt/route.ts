ade0e10b34c4c76e7fe5874e3ede3963765e35d7
Rismaludin Syah <rismaludin@gmail.com>
2025-08-27 20:59:06 +0700

    ads.txt

--- app/ads.txt/route.ts
+++ app/ads.txt/route.ts
@@ -2,19 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   // Generate ads.txt content
-  const adsContent = `# ads.txt file for apkmory.com
-# This file is used by advertising systems to verify authorized sellers
-
-# Google AdSense
-google.com, pub-6465174352180458, DIRECT, f08c47fec0942fa0
-
-# Add other ad networks here as needed
-# Format: <domain>, <publisher_id>, <relationship>, <certification_authority_id>
-# Example:
-# adsystem.com, 1234567890, DIRECT
-# adsystem.com, 0987654321, RESELLER
-
-# Note: Update publisher IDs when you add new ad networks`;
+  const adsContent = `google.com, pub-6465174352180458, DIRECT, f08c47fec0942fa0`;
 
   return new NextResponse(adsContent, {
     headers: {
